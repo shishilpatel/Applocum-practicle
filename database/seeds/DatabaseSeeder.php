40865bf81cfeb28e9a0ae745101800bc68ef3adf
Trick Informatics <shishil.patel@ymail.com>
2019-11-11 03:38:05 +0530

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(ConnectRelationshipsSeeder::class);
-        $this->call(ThemesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 
         Model::reguard();
